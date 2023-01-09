c453ec2b32c11a094af18f3d5286e4ad553b5e21
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-01-09 19:37:53 +0200

    Change function to class in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import React from 'react';
 import Calc from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Calc />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Calc />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
